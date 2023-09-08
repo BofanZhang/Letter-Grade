c5691ee63e628f4cc563bebf230d67f9fdaf9dc0
Bofan Zhang <124703355+BofanZhang@users.noreply.github.com>
2023-09-08 13:36:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 // Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 
-let mathletter = "";
 // Get Letter Grade:
 function btnClicked() {
-  let mathletter = getMathLetter;
+  let msg1 = getMathLetter("CS");
+}
+
+function getMathLetter(input) {
+  let element = document.getElementById(input);
+  let msg = "";
+  if (element.value > 5) {
+    msg = "F";
+    console.log(msg);
+  }
 }
