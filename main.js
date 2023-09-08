9a9f4d462b063921621a13f3baf5425ed9be1cff
Bofan Zhang <124703355+BofanZhang@users.noreply.github.com>
2023-09-08 12:54:14 -0600

    main.js

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
 // Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 
+let mathletter = "";
+// Get Letter Grade:
 function btnClicked() {
-  // Calculate Inputs
-  LetterGrade(
-    document.getElementById("CS").value,
-    document.getElementById("output").innerHTML
-  );
-  LetterGrade(document.getElementById("SP1").value);
-  LetterGrade(document.getElementById("SP2").value);
-  LetterGrade(document.getElementById("CS2").value);
-  LetterGrade(document.getElementById("ProjectA").value);
-}
-
-function LetterGrade(x, y, z) {
-  if (x >= 0 && x <= 49) {
-    console.log("F");
-    document.getElementById(y);
-  } else if (x >= 50 && x <= 59) {
-    console.log("D");
-  } else if (x >= 60 && x <= 74) {
-    console.log("C");
-  } else if (x >= 75 && x <= 89) {
-    console.log("B");
-  } else if (x >= 90 && x == 100) {
-    console.log("A");
-  }
+  let mathletter = getMathLetter;
 }
