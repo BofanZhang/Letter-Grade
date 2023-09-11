6032bd318d234548b087b0e870a997f4cdf6f654
Bofan Zhang <124703355+BofanZhang@users.noreply.github.com>
2023-09-11 13:25:58 -0600

    main.js && style.css

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
+// CS20 Grade Calculator by Bofan Zhang
+
 // Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 // Get Letter Grade:
 function btnClicked() {
+  // Calculate Client Side Scripting Grade (Part 1)
   let msg1 = getMathLetter("CS");
   document.getElementById("output").innerHTML = msg1;
+  // Calculate Structured Programing Grade (Part 1)
   let msg2 = getMathLetter("SP1");
   document.getElementById("output1").innerHTML = msg2;
+  // Calculate Structured Programing Grade (Part 2)
   let msg3 = getMathLetter("SP2");
   document.getElementById("output2").innerHTML = msg3;
+  // Calculate Client Side Scripting Grade (Part 2)
   let msg4 = getMathLetter("CS2");
   document.getElementById("output3").innerHTML = msg4;
+  // Calculate Project Grade
   let msg5 = getMathLetter("ProjectA");
   document.getElementById("output4").innerHTML = msg5;
 
+  // Calculate Final Percentage
   let AverageNum = calculateFinalPercent("CS", "SP1", "SP2", "CS2", "ProjectA");
   document.getElementById("output5").innerHTML = AverageNum + "%";
 
+  // Calculate Final Letter Grade
   let AverageLet = calculateFinalLet("CS", "SP1", "SP2", "CS2", "ProjectA");
   document.getElementById("output6").innerHTML = AverageLet;
 }
 
+// Calculate Letter Grade By User Input
 function getMathLetter(input) {
   let element = document.getElementById(input);
   if (element.value <= 49) {
@@ -36,6 +46,7 @@ function getMathLetter(input) {
   }
 }
 
+// Calculate Total Average Letter Grade by User Input
 function calculateFinalLet(input, input1, input2, input3, input4) {
   let total = Math.round(
     (+document.getElementById(input).value +
@@ -57,6 +68,8 @@ function calculateFinalLet(input, input1, input2, input3, input4) {
     return "A";
   }
 }
+
+// Calculate Total Average Number by User Input
 function calculateFinalPercent(input, input1, input2, input3, input4) {
   let total = Math.round(
     (+document.getElementById(input).value +
