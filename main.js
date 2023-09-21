ff466a4903b71a414a0152872bb4080756b6914c
Bofan Zhang <124703355+BofanZhang@users.noreply.github.com>
2023-09-21 12:33:51 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,30 +5,23 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 
 // Get Letter Grade:
 function btnClicked() {
-  // Calculate Client Side Scripting Grade (Part 1)
   let msg1 = getMathLetter("CS");
   Output("output").innerHTML = msg1;
-  // Calculate Structured Programing Grade (Part 1)
+
   let msg2 = getMathLetter("SP1");
   Output("output1").innerHTML = msg2;
-  // Calculate Structured Programing Grade (Part 2)
+
   let msg3 = getMathLetter("SP2");
   Output("output2").innerHTML = msg3;
-  // Calculate Client Side Scripting Grade (Part 2)
+
   let msg4 = getMathLetter("CS2");
   Output("output3").innerHTML = msg4;
-  // Calculate Project Grade
+
   let msg5 = getMathLetter("ProjectA");
   Output("output4").innerHTML = msg5;
 
   // Calculate Final Percentage
-  let AverageNumber =
-    (+document.getElementById("CS").value +
-      +document.getElementById("CS2").value +
-      +document.getElementById("SP1").value +
-      +document.getElementById("SP2").value +
-      +document.getElementById("ProjectA").value) /
-    5;
+  let AverageNumber = (+Output("CS").value + +Output("CS2").value + +Output("SP1").value + +Output("SP2").value + +Output("ProjectA").value) / 5;
   let msg6 = AverageNum(AverageNumber);
   Output("output6").innerHTML = msg6;
   Output("output5").innerHTML = AverageNumber + "%";
